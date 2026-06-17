cd8436f7315e9c954dee1c26dca25c27a7c84f81
Abel Gideon <abelgideontk7@gmail.com>
2026-06-17 11:37:09 +0300

    feat: Create bare-bone landing page

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,20 @@
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+import { ReactNode } from "react";
+
+export default function AuthLayout({ children }: { children: ReactNode }) {
+  return (
+    <div>
+      <Link
+        className={cn(buttonVariants({ variant: "secondary" }), "self-start")}
+        href="/"
+      >
+        <ArrowLeft />
+        Back
+      </Link>
+      <div>{children}</div>
+    </div>
+  );
+}
---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,39 @@
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+
+export default function HomePage() {
+  return (
+    <div className="flex flex-col space-y-40">
+      <nav className="flex justify-around">
+        <h1 className="text-2xl font-bold">Logo</h1>
+        <div className="flex space-x-3">
+          <Link className={buttonVariants()} href={"/signup"}>
+            Sign up
+          </Link>
+          <Link
+            className={buttonVariants({ variant: "secondary" })}
+            href={"/login"}
+          >
+            Log in
+          </Link>
+        </div>
+      </nav>
+      <main className="text-center">
+        <h1 className="text-6xl font-extrabold uppercase tracking-tight leading-[0.9] text-foreground">
+          LEARN FROM YOUTUBE
+        </h1>
+        <h1 className="text-6xl font-extrabold uppercase tracking-tight leading-[0.9] text-muted-foreground">
+          DISTRACTION FREE
+        </h1>
+        <p className="mt-3 text-lg text-muted-foreground max-w-md mx-auto">
+          Store your courses and Track your progress all in one place.
+        </p>
+        <Link className={cn(buttonVariants(), "mt-7")} href={"/signup"}>
+          Get Started Now <ArrowRight />
+        </Link>
+      </main>
+    </div>
+  );
+}
