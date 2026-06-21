6228b757202f233bfb64fca5c70d21703089d81b
Abel Gideon <abelgideontk7@gmail.com>
2026-06-21 19:24:04 +0300

    feat: Add logo to navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -10,9 +10,15 @@ export default function Navbar() {
   const { data, isPending } = useSession();
   return (
     <header className="sticky top-0 z-50 border-b bg-background">
-      <nav className="flex flex-col gap-3 md:flex-row md:gap-0 items-center justify-between max-w-7xl mx-auto px-4 py-4">
-        <Link href="/" className="text-2xl font-bold">
-          Logo
+      <nav className="flex flex-col gap-4 md:flex-row md:gap-0 items-center justify-between max-w-7xl mx-auto px-4 py-4">
+        <Link
+          href="/"
+          className="m-3 md:m-0 text-2xl font-bold flex justify-center items-center"
+        >
+          <p>
+            <span className="dark:text-white text-[20px]">course</span>
+            <span className="text-amber-700 text-[20px]">compiler</span>
+          </p>
         </Link>
         <div className="flex items-center space-x-3">
           {isPending ? (
